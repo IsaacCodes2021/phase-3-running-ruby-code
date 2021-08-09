8afe586fc5170ff08e84368d0a7fe7e3104f7923
IsaacCodes2021 <84209008+IsaacCodes2021@users.noreply.github.com>
2021-08-08 22:37:52 -0700

    commit tree 1d2f73fe3bf6ac41225b54ed0caa8fa2e33a1ca1

---
+++ app.rb
@@ -0,0 +1,12 @@
+#app.rb
+
+puts "Hello World!"
+puts "Hello World"
+puts "Hello World"
+p [1, 2, 3]
+print "Hello World!"
+print "Hello World"
+print "Hello World"
+puts "Pass this test, please."
+#pretty print
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
